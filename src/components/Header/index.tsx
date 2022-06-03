31841b96e8251bbae9389b01d9e4d27023c4ba3c
Estevão <estevaog.gomes@gmail.com>
2022-06-03 15:55:56 -0300

    Added callenge steps 1 to 3

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
+
+import logo from '../../assets/images/logo.svg';
+import { Container, Cart } from './styles';
+import { useCart } from '../../hooks/useCart';
+
+const Header = (): JSX.Element => {
+  const { cart } = useCart();
+  const cartSize = cart.reduce((size, cartProduct, index)=>
+      //Testa se o produto atual é o primeiro produto na array com id atual 
+      index === cart.findIndex((searchedProduct)=>searchedProduct.id === cartProduct.id) ? size + 1 : size
+  , 0)
+
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span data-testid="cart-size">
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
+          </span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
+};
+
+export default Header;
